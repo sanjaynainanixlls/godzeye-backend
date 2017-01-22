120b910f4d99416814548d84c50d6201f9e4aae6
Aman Kumar <aman.kumar@girnarsoft.com>
2017-01-22 23:51:23 +0530

    repo changes

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,4 +1,10 @@
 <?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 include_once (dirname(__FILE__) . "/connection.php");
 include_once (dirname(dirname(__FILE__)) . "/common/Functions.php");
 include_once (dirname(dirname(__FILE__)) . "/common/functions.common.php");
--- common/functions.common.php
+++ common/functions.common.php
@@ -8,53 +8,10 @@ function debug($val) {
     print_r($val);
 }
 
+//get all details of Institutes
 function getInstituteList() {
-    $query = "SELECT id,institute FROM institutions WHERE status=1";
+    $query = "SELECT * FROM institutions WHERE status=1";
     $result = queryRunner::doSelect($query);
     return $result;
 }
 
-function getFileDelimiter($file, $checkLines = 2) {
-    //$file = new SplFileObject($file);
-    $delimiters = array(
-        'comma' => ',',
-        'tab' => '\t',
-        'semicolon' => ';',
-        'sep' => '|',
-        'colon' => ':'
-    );
-    $results = array();
-    //for($i = 0; $i <= $checkLines; $i++){
-    $line = fgetcsv($file);
-    $rawLine = $line;
-    $count = 0;
-    foreach ($delimiters as $key => $delimiter) {
-        $regExp = '/[' . $delimiter . ']/';
-        $fields = preg_split($regExp, $line[0]);
-        if (count($fields) > 1) {
-            if (!empty($results[$delimiter])) {
-                $results[$delimiter] ++;
-            } else {
-                $results[$key] = 1;
-            }
-        }
-        $count++;
-    }
-    //}
-    $results = array_keys($results, max($results));
-    return array('delem' => $results[0], 'rawCols' => $rawLine);
-}
-
-function filterArrayKeysSpace($array) {
-    $response = array();
-    foreach ($array as $key => $value) {
-        $response[] = trim($value);
-    }
-    return $response;
-}
-
-function getTestNameAndId(){
-    $query = "SELECT id,test_name FROM tests";
-    $result = queryRunner::doSelect($query);
-    return $result;
-}
