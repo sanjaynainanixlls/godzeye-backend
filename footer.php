388ac5ca8157eb393b15f8a4101011198fa890e5
Aman Kumar <aman.kumar@girnarsoft.com>
2017-01-23 01:05:37 +0530

    added hdr N ftr

---
+++ footer.php
@@ -0,0 +1,96 @@
+<!-- Call to Action Box -->
+<div class="call-to-action-box" data-parallax-bg="img/call-to-action-bg.jpg">
+    <div class="box-img"><span></span></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>Enjoy our courses Today</h2>
+            </div>
+
+            <div class="col-md-5">
+                <div class="button-wrapper">
+                    <a href="#" class="btn theme-btn-3">Get in touch</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<!-- Footer -->
+<footer class="fixed">
+    <div class="container">
+        <div class="footer-wrapper">
+            <img src="img/director.png" alt="footer brand" class="footer-brand" />
+
+            <ul class="social-block">
+                <li><a href="https://twitter.com/TeslaThemes"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="https://dribbble.com/TeslaThemes/"><i class="fa fa-dribbble"></i></a></li>
+                <li><a href="https://www.facebook.com/TeslaThemes"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://www.pinterest.com/teslathemes/"><i class="fa fa-pinterest"></i></a></li>
+                <li><a href="https://www.facebook.com/TeslaThemes/photos_stream"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.linkedin.com/grp/home?gid=5039596"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+
+            <div class="main-area">
+                <div class="menu">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="courses-grid.html">Courses</a></li>
+                        <li><a href="pricing.html">Pricing</a></li>
+                        <li><a href="course.html">Single Course</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="footer-widget widget_info">
+                            <p><span>Dr. Edward B. Smith</span><br />Superintendent of Public<br />Instruction</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="footer-widget widget_contact">
+                            <p>Braavos Department of Education<br />877-644-6338 |<br /><a href="#">contact@courses.com</a></p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="footer-widget widget_tagcloud">
+                            <div class="tagcloud">
+                                <a href="#">Education</a>
+                                <a href="#">Courses</a>
+                                <a href="#">Design</a>
+                                <a href="#">Health</a>
+                                <a href="#">Hospitality</a>
+                                <a href="#">Business</a>
+                                <a href="#">Instruction</a>
+                                <a href="#">Skills</a>
+                                <a href="#">Professor</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="copyrights">
+                <p>Copyright 2015. Designed by <a href="http://www.teslathemes.com" target="blank">TeslaThemes</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+
+<!-- Scripts -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/lightbox.js"></script>
+<script src="js/velocity.js"></script>
+<script src="js/modernizr.js"></script>
+<script src="js/smooth-scroll.js"></script>
+<script src="js/bxslider.js"></script>
+<script src="js/options.js"></script>
+</body>
+</html>
+
