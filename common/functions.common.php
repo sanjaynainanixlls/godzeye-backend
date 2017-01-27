d8d34f85bd7cad3c9b13f701ee414178c694b652
Aman Kumar <aman.kumar@girnarsoft.com>
2017-01-27 20:31:40 +0530

    updated from master

--- common/functions.common.php
+++ common/functions.common.php
@@ -33,7 +33,7 @@ function getInstituteData($id){
     
 }
 
-function getTeacherData($instituteId,$teacherId){
+function getTeacherData($instituteId='',$teacherId=''){
     $cond = '';
     if($instituteId != '' && $teacherId == ''){
         $cond = "AND institution_id='$instituteId'";
