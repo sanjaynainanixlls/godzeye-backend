1df4e696168d7277f3d3af44e17f9281b6789cab
Aman Kumar <aman.kumar@girnarsoft.com>
2017-01-28 16:36:13 +0530

    added auto

---
+++ common/autocomplete.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once '/config/config.php';
+class autocomplete{
+    public function execute($token){
+        $AutoObj = new autoCompleteWord();
+        $result = $AutoObj->getToken($token);
+        echo json_encode($result);
+    }
+}
+$token = $_GET['term'];
+$AutoCompleteObj = new autocomplete();
+$AutoCompleteObj->execute($token);
+?>
