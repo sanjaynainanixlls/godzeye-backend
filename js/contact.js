cf6e168aed2d533abd068a02c3882d409c4fb5fc
Aman Kumar <aman.kumar@girnarsoft.com>
2017-02-07 22:05:20 +0530

    added js file too

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+jQuery('#submit_form').click(function () {
+    var username = $('#sername').val();
+    var email = $('#email').val();
+    var mobile = $('#mobile').val();
+    var message = $('#message').val();
+    if (username != '' && email != '' && mobile != '' && message != '') {
+        $.ajax({
+            url: 'common/ajax.function.php',
+            method: 'POST',
+            data: 'action=contact-form&username=' + username + '&email=' + email + '&mobile=' + mobile + '&message=' + message
+        }).done(function (data) {
+            var msg = $.parseJSON(data);
+            if(msg){
+                alert(msg.message);
+            }
+        });
+    }
+});
